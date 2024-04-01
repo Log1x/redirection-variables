81872dbd43db027e25b262fef32112769e71a258
Brandon <brandon@tendency.me>
2024-04-01 12:33:03 -0500

    🩹 Fix plugin autoloader

--- plugin.php
+++ plugin.php
@@ -4,13 +4,17 @@
  * Plugin Name: Redirection Variables
  * Plugin URI:  https://github.com/log1x/redirection-variables
  * Description: Provides referer tracking variables for popular WordPress redirection plugins.
- * Version:     1.0.3
+ * Version:     1.0.4
  * Author:      Brandon Nifong
  * Author URI:  https://github.com/log1x
  * Licence:     MIT
  */
 
 namespace Log1x\Plugin\RedirectionVariables;
+
+if (file_exists($composer = __DIR__.'/vendor/autoload.php')) {
+    require_once $composer;
+}
 
 add_action('after_setup_theme', new class
 {
@@ -21,10 +25,6 @@ add_action('after_setup_theme', new class
      */
     public function __invoke()
     {
-        require_once file_exists($composer = __DIR__.'/vendor/autoload.php') ?
-            $composer :
-            __DIR__.'/dist/autoload.php';
-
         return new RedirectionVariables(
             plugin_dir_path(__FILE__),
             plugin_dir_url(__FILE__)
